7b7094f0611a083677003876cea806dfce70f960
pg185155 <pg185155@ncr.com>
2021-09-07 10:35:04 +0530

    Re-configured config.rb file

--- .chef/config.rb
+++ .chef/config.rb
@@ -2,8 +2,8 @@
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "gaActions"
-client_key               "#{current_dir}/validator.pem"
+node_name                "prasanth155518"
+client_key               "#{current_dir}/prasanth155518.pem"
 chef_server_url          "https://api.chef.io/organizations/chef_training155"
 cookbook_path            ["../"]
 knife[:editor] = "/usr/bin/vim"
